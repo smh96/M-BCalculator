56b72c09056df56613b1db2140f75497993b0283
Sander Haug <sanderhaug1@Sanders-MacBook-Pro.local>
2017-07-20 19:46:57 -0700

    Initial Commit

---
+++ M&B Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  M&B Calculator
+//
+//  Created by Sander Haug on 20/07/2017.
+//  Copyright © 2017 Sander Haug. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
